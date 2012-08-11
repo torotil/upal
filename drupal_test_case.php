2cf377b5dc9f5cc32d510342bfc1eb72f879b255
Chris Calip <chriscalip@yahoo.com>
2012-08-11 03:02:28 -0500

    #2 resynch setup.

--- drupal_test_case.php
+++ drupal_test_case.php
@@ -2392,7 +2392,7 @@ class DrupalUnitTestCase extends DrupalTestCase {
   function setUp() {
     upal_init();        
     if (!defined("DRUPAL_ROOT")) {
-      define('DRUPAL_ROOT', dirname(__FILE__));
+      define('DRUPAL_ROOT', UPAL_ROOT);
     }    
     if (!defined("DRUPAL_CORE_VERSION")) {
       define('DRUPAL_CORE_VERSION', "7");
@@ -2413,7 +2413,7 @@ class DrupalWebTestCase extends DrupalTestCase {
   public function setUp() {
     upal_init();    
     if (!defined("DRUPAL_ROOT")) {
-      define('DRUPAL_ROOT', dirname(__FILE__));
+      define('DRUPAL_ROOT', UPAL_ROOT);
     }
     if (!defined('DRUPAL_CORE_VERSION')) {
 	     define('DRUPAL_CORE_VERSION', "7");
@@ -2437,23 +2437,52 @@ class DrupalWebTestCase extends DrupalTestCase {
  * Initialize our environment at the start of each run (i.e. suite).
  */
 function upal_init() {
-  define('DRUPAL_CORE_VERSION', getenv('DRUPAL_CORE_VERSION') ? getenv('DRUPAL_CORE_VERSION') :(isset($GLOBALS['DRUPAL_CORE_VERSION']) ? $GLOBALS['DRUPAL_CORE_VERSION'] : "7"));
-  define('DRUPAL_SITE_URL', getenv('DRUPAL_SITE_URL') ? getenv('DRUPAL_SITE_URL') :(isset($GLOBALS['DRUPAL_SITE_URL']) ? $GLOBALS['DRUPAL_SITE_URL'] : ''));
+  // UNISH_DRUSH value can come from phpunit.xml or `which drush`.
+  if (!defined('UNISH_DRUSH')) {
+    // Let the UNISH_DRUSH environment variable override if set.
+    $unish_drush = isset($_SERVER['UNISH_DRUSH']) ? $_SERVER['UNISH_DRUSH'] : NULL;
+    $unish_drush = isset($GLOBALS['UNISH_DRUSH']) ? $GLOBALS['UNISH_DRUSH'] : $unish_drush;
+    if (empty($unish_drush)) {
+      // $unish_drush = Drush_TestCase::is_windows() ? exec('for %i in (drush) do @echo.   %~$PATH:i') : trim(`which drush`);
+      $unish_drush = trim(`which drush`);
+    }
+    define('UNISH_DRUSH', $unish_drush);
+  }
+
+  // We read from globals here because env can be empty and ini did not work in quick test.
+  define('UPAL_DB_URL', getenv('UPAL_DB_URL') ? getenv('UPAL_DB_URL') : (!empty($GLOBALS['UPAL_DB_URL']) ? $GLOBALS['UPAL_DB_URL'] : 'mysql://root:@127.0.0.1/upal'));
+
+  // Make sure we use the right Drupal codebase.
+  define('UPAL_ROOT', getenv('UPAL_ROOT') ? getenv('UPAL_ROOT') : (isset($GLOBALS['UPAL_ROOT']) ? $GLOBALS['UPAL_ROOT'] : realpath('.')));
+  chdir(UPAL_ROOT);
+
+  // The URL that browser based tests should use.
+  define('UPAL_WEB_URL', getenv('UPAL_WEB_URL') ? getenv('UPAL_WEB_URL') : (isset($GLOBALS['UPAL_WEB_URL']) ? $GLOBALS['UPAL_WEB_URL'] : 'http://upal'));
+
+  define('DRUPAL_CORE_VERSION', getenv('DRUPAL_CORE_VERSION') ? getenv('DRUPAL_CORE_VERSION') :(isset($GLOBALS['DRUPAL_CORE_VERSION']) ? $GLOBALS['DRUPAL_CORE_VERSION'] : "8"));
+
+  define('UPAL_TMP', getenv('UPAL_TMP') ? getenv('UPAL_TMP') : (isset($GLOBALS['UPAL_TMP']) ? $GLOBALS['UPAL_TMP'] : sys_get_temp_dir()));
+  // define('UNISH_SANDBOX', UNISH_TMP . '/drush-sandbox');
+
   // Set the env vars that Drupal expects. Largely copied from drush.
-  $url = parse_url(DRUPAL_SITE_URL);
+  $url = parse_url(UPAL_WEB_URL);
+
   if (array_key_exists('path', $url)) {
     $_SERVER['PHP_SELF'] = $url['path'] . '/index.php';
   }
   else {
     $_SERVER['PHP_SELF'] = '/index.php';
-  }  
-  $_SERVER['HTTP_HOST'] = DRUPAL_SITE_URL;
-  $_SERVER['PHP_SELF'] =  '/index.php';
+  }
+
   $_SERVER['REQUEST_URI'] = $_SERVER['SCRIPT_NAME'] = $_SERVER['PHP_SELF'];
   $_SERVER['REMOTE_ADDR'] = '127.0.0.1';
   $_SERVER['REQUEST_METHOD']  = NULL;
+
   $_SERVER['SERVER_SOFTWARE'] = NULL;
   $_SERVER['HTTP_USER_AGENT'] = NULL;
+
+  $_SERVER['HTTP_HOST'] = $url['host'];
+  $_SERVER['SERVER_PORT'] = array_key_exists('port', $url) ? $url['port'] : NULL;
 }
 
  // This code is in global scope.
