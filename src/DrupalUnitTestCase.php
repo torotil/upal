29cc88cab5b270a9b8e01bcc14632426fba57a15
Roman Zimmermann <roman@more-onion.com>
2018-11-20 21:04:13 +0100

    Set the current working directory to DRUPAL_ROOT.

--- src/DrupalUnitTestCase.php
+++ src/DrupalUnitTestCase.php
@@ -8,8 +8,17 @@ class DrupalUnitTestCase extends DrupalTestCase {
   /**
    * Do a full bootstrap of the site.
    */
-  function setUp() {
+  public function setUp() {
     DrupalBootstrap::bootstrap();
+    $this->oldCwd = getcwd();
+    chdir(DRUPAL_ROOT);
+  }
+
+  /**
+   * Reset the current working directory.
+   */
+  public function tearDown() {
+    chdir($this->oldCwd);
   }
 
 }
