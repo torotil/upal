cdf7aa5588169fccf730ee43d52b38b754d78271
Roman Zimmermann <roman@more-onion.com>
2019-09-29 14:44:34 +0200

    Add our own autoload to the bootstrap.php.

--- bootstrap.php
+++ bootstrap.php
@@ -5,6 +5,8 @@ namespace Upal;
 // Include our autoloader in case PHPUnit wasn’t called from the same
 // composer environment.
 @include_once(__DIR__ . '/../../autoload.php');
+// Include autoload in dev builds.
+@include_once(__DIR__ . '/vendor/autoload.php');
 
 // Define class aliases for backwards compatibility.
 class_alias(DrupalUnitTestCase::class, 'DrupalUnitTestCase');
