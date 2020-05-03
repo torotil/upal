93013cdc21cb68e50157323bc8b0e1a5a0616c13
Roman Zimmermann <roman@more-onion.com>
2020-05-03 09:43:23 +0200

    Copy drupal_bootstrap() to be able to modify it.

--- src/DrupalBootstrap.php
+++ src/DrupalBootstrap.php
@@ -56,8 +56,91 @@ class DrupalBootstrap {
     self::initializeDrupal();
     $old_dir = getcwd();
     chdir(UPAL_ROOT);
-    drupal_bootstrap($phase);
+    static::drupalBootstrap($phase);
     chdir($old_dir);
+  }
+
+  /**
+   * Go through Drupal’s bootstrap phases.
+   *
+   * This is a slightly modified copy of drupal_bootstrap().
+   */
+  protected static function drupalBootstrap($phase = NULL, $new_phase = TRUE) {
+    // Not drupal_static(), because does not depend on any run-time information.
+    static $phases = array(
+      DRUPAL_BOOTSTRAP_CONFIGURATION,
+      DRUPAL_BOOTSTRAP_PAGE_CACHE,
+      DRUPAL_BOOTSTRAP_DATABASE,
+      DRUPAL_BOOTSTRAP_VARIABLES,
+      DRUPAL_BOOTSTRAP_SESSION,
+      DRUPAL_BOOTSTRAP_PAGE_HEADER,
+      DRUPAL_BOOTSTRAP_LANGUAGE,
+      DRUPAL_BOOTSTRAP_FULL,
+    );
+    // Not drupal_static(), because the only legitimate API to control this is to
+    // call drupal_bootstrap() with a new phase parameter.
+    static $final_phase;
+    // Not drupal_static(), because it's impossible to roll back to an earlier
+    // bootstrap state.
+    static $stored_phase = -1;
+
+    if (isset($phase)) {
+      // When not recursing, store the phase name so it's not forgotten while
+      // recursing but take care of not going backwards.
+      if ($new_phase && $phase >= $stored_phase) {
+        $final_phase = $phase;
+      }
+
+      // Call a phase if it has not been called before and is below the requested
+      // phase.
+      while ($phases && $phase > $stored_phase && $final_phase > $stored_phase) {
+        $current_phase = array_shift($phases);
+
+        // This function is re-entrant. Only update the completed phase when the
+        // current call actually resulted in a progress in the bootstrap process.
+        if ($current_phase > $stored_phase) {
+          $stored_phase = $current_phase;
+        }
+
+        switch ($current_phase) {
+          case DRUPAL_BOOTSTRAP_CONFIGURATION:
+            require_once DRUPAL_ROOT . '/includes/request-sanitizer.inc';
+            _drupal_bootstrap_configuration();
+            break;
+
+          case DRUPAL_BOOTSTRAP_PAGE_CACHE:
+            _drupal_bootstrap_page_cache();
+            break;
+
+          case DRUPAL_BOOTSTRAP_DATABASE:
+            _drupal_bootstrap_database();
+            break;
+
+          case DRUPAL_BOOTSTRAP_VARIABLES:
+            _drupal_bootstrap_variables();
+            break;
+
+          case DRUPAL_BOOTSTRAP_SESSION:
+            require_once DRUPAL_ROOT . '/' . variable_get('session_inc', 'includes/session.inc');
+            drupal_session_initialize();
+            break;
+
+          case DRUPAL_BOOTSTRAP_PAGE_HEADER:
+            _drupal_bootstrap_page_header();
+            break;
+
+          case DRUPAL_BOOTSTRAP_LANGUAGE:
+            drupal_language_initialize();
+            break;
+
+          case DRUPAL_BOOTSTRAP_FULL:
+            require_once DRUPAL_ROOT . '/includes/common.inc';
+            _drupal_bootstrap_full();
+            break;
+        }
+      }
+    }
+    return $stored_phase;
   }
 
   public static function backupDatabase() {
